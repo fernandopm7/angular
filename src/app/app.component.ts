efeee260a85a8527dde03738d8a93677419b702e
samoranax\fernando <fer_nan28@hotmail.com>
2020-12-31 13:19:27 +0100

    first cmmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'hello angular 10';
+  description = 'holaaa';
+  hero='Superman';
+
+  onLike() {
+    window.alert(`I like ${this.hero}`);
+}
 }
---
+++ src/app/prueba.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'prueba-selector',
+  templateUrl: 'name.component.html'
+})
+
+export class PruebaComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
